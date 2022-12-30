62adde0c4fc02684a06e08b3973ec51bd2142d5e
Josh Zhang <114123021+itsjoshzhang@users.noreply.github.com>
2022-12-29 17:24:23 -0800

    Add files via upload

--- TasteBuddies10/Login/Splash.swift
+++ TasteBuddies10/Login/Splash.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct Splash: View {
+    
     @State private var showOnboard = false
     @State private var size = 0.9
     @State private var opacity = 0.0
@@ -10,6 +11,7 @@ struct Splash: View {
         VStack {
             VStack {
                 Image("logo")
+                
                 Text("Taste Buddies")
                     .bold()
                     .font(.custom("pacifico", size: 48))
@@ -22,14 +24,14 @@ struct Splash: View {
             .scaleEffect(size)
             .opacity(opacity)
             .onAppear {
-                withAnimation(.easeIn(duration: 1.0)) {
+                withAnimation(.easeIn(duration: 1)) {
                     size = 1.0
                     opacity = 1.0
                 }
             }
         }
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                 withAnimation {
                     showOnboard = true
                 }
@@ -40,8 +42,59 @@ struct Splash: View {
         }
     }
 }
+struct SplashOrder: View {
+    
+    @State private var showSuggest = false
+    @State private var size = 0.9
+    @State private var opacity = 0.0
+    @State private var progress = 0.0
+    
+    var timer = Timer.publish(
+        every: 0.01, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        VStack {
+            Image("logo")
+            
+            Text("Taste Buddies")
+                .font(.custom("pacifico", size: 48))
+                .foregroundColor(.pink)
+            
+            Text("Loading the perfect dish...")
+                .font(.headline)
+                .foregroundColor(.pink)
+            
+            ProgressView(value: progress, total: 180)
+                .padding()
+                .background(.gray.opacity(0.25))
+                .cornerRadius(8)
+                .tint(.pink)
+                .padding()
+            
+                .onReceive(timer) { _ in
+                    if progress < 300 {
+                        progress += 1
+                    } else {
+                        showSuggest = true
+                    }
+                }
+        }
+        .scaleEffect(size)
+        .opacity(opacity)
+        .onAppear {
+            withAnimation(.easeIn(duration: 1.0)) {
+                self.size = 1.0
+                self.opacity = 1.0
+            }
+        }
+        .fullScreenCover(isPresented: $showSuggest) {
+            Order()
+        }
+    }
+}
 struct Splash_Previews: PreviewProvider {
     static var previews: some View {
         Splash()
+        SplashOrder()
     }
 }
