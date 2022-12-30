43893809b454cc87ae367a829c6a4cdf7c08c0c8
Josh Zhang <114123021+itsjoshzhang@users.noreply.github.com>
2022-12-29 17:32:08 -0800

    Add files via upload

---
+++ TasteBuddies10/Location/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  TasteBuddies10
+//
+//  Created by Josh Zhang on 12/29/22.
+//
+
+import SwiftUI
+
+struct Location: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Location_Previews: PreviewProvider {
+    static var previews: some View {
+        Location()
+    }
+}
